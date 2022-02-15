b7b604be08deddf673b52e653b7e3ce415210fa0
isa <isabe.dulischewski@icloud.com>
2022-02-15 16:06:30 +0100

    Added printMatchField Method and the Method generateSeparatorBar

--- src/main/java/de/tictactoe/MatchField.java
+++ src/main/java/de/tictactoe/MatchField.java
@@ -6,17 +6,20 @@ public class MatchField {
     private int rows = 3;
     private int columns = 3;
     char[][] symbols;
+    private String separationBar;
 
     //Konstruktor
     public MatchField(){
         this.rows = rows + 1;
         this.columns = columns + 1;
+        this.separationBar = generateSeparatorBar();
 
         symbols = new char[this.rows][this.columns];
 
     }
 
-    public void printMatchField (){
+    //Method to print the MatchField
+    public void printMatchField () {
         for(int y = 0; y < rows; y++){
             for(int x = 0; x < columns; x++){
 
@@ -37,15 +40,18 @@ public class MatchField {
                 }
             }
             System.out.println();
-            //
+            System.out.println(separationBar);
         }
-
-
-
-
     }
 
-
+    //Auxiliary method to generate separator bars flexibly
+    public String generateSeparatorBar() {
+        String separator = "---+";
+        for(int x = 1; x < columns; x++){
+            separator = separator + "---+";
+        }
+        return separator;
+    }
 
 
 
